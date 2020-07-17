1c66c21f925a4988798fd68d62a96464710fcd00
HarshMantri <harshmantri98@gmail.com>
2020-07-18 02:20:34 +0530

    added multiprocessing question code

--- multiprocessing_proj/generator.py
+++ multiprocessing_proj/generator.py
@@ -2,4 +2,11 @@ import redis
 import random
 import uuid
 
+def main():
+    r = redis.Redis(host='localhost', port=6379)
+    while True:
+        r.zincrby('queue', 1, str(uuid.uuid1()))
+
+if __name__ == "__main__":
+    main()
 
