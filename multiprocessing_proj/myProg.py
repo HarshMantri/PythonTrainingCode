f9062cf88db53365677a2b0b0caf6d6dded9e21d
HarshMantri <harshmantri98@gmail.com>
2020-07-18 18:26:24 +0530

    Final multi threaded code for pushing redis responses to server synchronously

--- multiprocessing_proj/myProg.py
+++ multiprocessing_proj/myProg.py
@@ -4,20 +4,23 @@ import requests
 
 endpoint = "http://127.0.0.1:8000"
 
-result = requests.get(endpoint)
-print(result)
-
+#result = requests.get(endpoint)
+#print(result)
 
 redis = redis.Redis(host="localhost", port = 6379)
+
+def msg_server(server, data):
+    resp = requests.get(server, data)
+    print(f'{resp} is the server response for {data} being pushed')
 
 while True:
     try:
         data = redis.zrange('queue',-1,-1)
         if len(data):
-            print(data[0])
+            #print(data[0])
             redis.zrem('queue',data[0])
-            resp = requests.get(endpoint, data[0])
-            print(resp)
+            t = threading.Thread(target = msg_server, args = [endpoint, data[0]])
+            t.start()
     except:
         print('ERROR!')
         pass
